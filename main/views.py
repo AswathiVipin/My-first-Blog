b291677007dac9ac0fca71cd3b330ff64cdaf45d
AswathiVipin <aswathivipi2016@gmail.com>
2026-05-21 12:28:51 +0530

    test file add, settings updtaed

--- main/views.py
+++ main/views.py
@@ -24,3 +24,5 @@ def home(request):
    return render(request, "home.html", {"posts": posts})
 def about(request):
     return render(request, "about.html")
+def contact(request):
+    return render(request, "contact.html")
