9cf10eb16f72ab158555b166b358ea85871c1fc3
AswathiVipin <aswathivipi2016@gmail.com>
2026-05-21 12:10:40 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,26 @@
+
+from django.shortcuts import render
+
+
+def home(request):
+   posts = [
+   {
+   "title": "First Blog Post",
+   "content": "This is my first Django blog post."
+   },
+   {
+    "title": "Learning Django",
+    "content": "Django makes web development easy."
+   },
+   {
+    "title": "Templates in Django",
+    "content": "Django templates help separate logic and HTML."
+   },
+   {
+    "title": "Templates in Django",
+    "content": "Django templates help separate logic and HTML."
+   }
+  ]
+   return render(request, "home.html", {"posts": posts})
+def about(request):
+    return render(request, "about.html")
